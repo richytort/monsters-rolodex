00b6f5daedeb708fd310f4eaa0c88b6d8fa4ffc9
richytort <47453583+richytort@users.noreply.github.com>
2020-09-24 10:42:18 -0700

    Updates

---
+++ precache-manifest.b2f0ce4a33d3fa92bcb29e0bba467009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1543e8c38e4171a1e452b1ec9d61e9c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "506d81cbd5dd8069c574",
+    "url": "/monsters-rolodex/static/css/main.a40a0e1f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506d81cbd5dd8069c574",
+    "url": "/monsters-rolodex/static/js/main.422e448f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
